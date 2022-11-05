e3117cdb53b37966527aa40be80419ce8af27506
leticea <nascimento.leticeah@gmail.com>
2022-11-05 16:45:42 -0300

    criando evento para pegar valores do input

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const pokemonName = document.querySelector('.pokemon-name');
 const pokemonNumber = document.querySelector('.pokemon-number');
 const pokemonImage = document.querySelector('.pokemon-image');
 
+const form = document.querySelector('.form');
+
 const fetchPokemon = async (pokemon) => {
 
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -18,4 +20,10 @@ const renderPokemon = async (pokemon) => {
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 }
 
-renderPokemon('6');
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+
+    console.log('enviando formulario...');
+
+});
