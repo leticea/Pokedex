e9ccb8f438d8d430bb4fadbd955a05bfbee029ee
leticea <nascimento.leticeah@gmail.com>
2022-11-05 16:55:37 -0300

    criando evento para pegar valores do input e mostrar os pokemons

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const input = document.querySelector('.input-search');
 
 const fetchPokemon = async (pokemon) => {
 
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
     const data = await APIResponse.json();
     return data;
 }
@@ -19,11 +19,12 @@ const renderPokemon = async (pokemon) => {
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
+    input.value = '';
 }
 
 form.addEventListener('submit', (event) => {
 
     event.preventDefault();
-
-    renderPokemon(input.value);
+    renderPokemon(input.value);    
 });
